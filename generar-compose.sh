669da404af8a4560b16d8aa0f2fa8b3073cee9d2
MateoV <vroonlandmateo@gmail.com>
2025-03-17 18:48:27 -0300

    fix: change file name to spanish

--- generar-compose.sh
+++ generar-compose.sh
@@ -6,4 +6,4 @@ NUM_CLIENTS=$2
 echo "Nombre del archivo de salida: $OUTPUT_FILE"
 echo "Cantidad de clientes: $NUM_CLIENTS"
 
-python3 generator.py $OUTPUT_FILE $NUM_CLIENTS
+python3 mi-generador.py $OUTPUT_FILE $NUM_CLIENTS
