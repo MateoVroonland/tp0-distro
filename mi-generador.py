291232b7e38d475d06a59347c81d4e30541c86a1
MateoV <vroonlandmateo@gmail.com>
2025-03-18 01:08:06 -0300

    feat: bind mount config archives

--- mi-generador.py
+++ mi-generador.py
@@ -13,6 +13,8 @@ services:
       - LOGGING_LEVEL=DEBUG
     networks:
       - testing_net
+    volumes:
+      - ./server/config.ini:/config.ini
 """
 
     for i in range(1, num_clients + 1):
@@ -27,6 +29,8 @@ services:
       - testing_net
     depends_on:
       - server
+    volumes:
+      - ./client/config.yaml:/config.yaml
 """
         compose_content += client_section
 
