5fd7db654ebac1de5095fc5f52e78da0d293806c
MateoV <vroonlandmateo@gmail.com>
2025-03-22 13:33:37 -0300

    feat: add env variables for each client

--- mi-generador.py
+++ mi-generador.py
@@ -1,5 +1,31 @@
 
 import sys
+import random
+from datetime import datetime, timedelta
+
+def generate_random_name():
+    first_names = ["Enzo", "Emiliano", "Julian", "Lionel", "Mateo"]
+    return random.choice(first_names)
+
+def generate_random_surname():
+    surnames = ["Messi", "Rodriguez", "Martinez", "Alvarez", "Fernandez"]
+    return random.choice(surnames)
+
+def generate_random_document():
+    return str(random.randint(10000000, 55000000))
+
+def generate_random_birthdate():
+    start_date = datetime(1980, 1, 1)
+    end_date = datetime(2024, 12, 31)
+    
+    delta_days = (end_date - start_date).days
+    random_days = random.randint(0, delta_days)
+    random_date = start_date + timedelta(days=random_days)
+    
+    return random_date.strftime("%Y-%m-%d")
+
+def generate_random_bet_number():
+    return str(random.randint(0, 9999)).zfill(4)
 
 def generate_compose(output_file, num_clients):
     compose_content = f"""name: tp0
@@ -17,12 +43,22 @@ services:
 """
 
     for i in range(1, num_clients + 1):
+        nombre = generate_random_name()
+        apellido = generate_random_surname()
+        documento = generate_random_document()
+        nacimiento = generate_random_birthdate()
+        numero = generate_random_bet_number()
         client_section = f"""  client{i}:
     container_name: client{i}
     image: client:latest
     entrypoint: /client
     environment:
       - CLI_ID={i}
+      - NOMBRE="{nombre}"
+      - APELLIDO="{apellido}"
+      - DOCUMENTO={documento}
+      - NACIMIENTO={nacimiento}
+      - NUMERO={numero}
     networks:
       - testing_net
     depends_on:
